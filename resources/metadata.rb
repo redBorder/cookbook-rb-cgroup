22d54655845c8ece82084a7414dcb010453c22d3
Malvads <malvarez@redborder.com>
2023-11-28 09:15:29 +0000

    Change mantainer name

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -1,6 +1,6 @@
 unified_mode     'true'
 name             'rbcgroup'
-maintainer       'redborder'
+maintainer       'Miguel Álvarez'
 maintainer_email 'malvarez@redborder.com'
 license          'AGPL-3.0'
 description      'Installs/Configures cookbook-rb-cgroup'
