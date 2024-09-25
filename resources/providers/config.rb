0108ee7f6e7204dc71f5cdd3d83ff3d45e323cca
Miguel Alvarez <malvarez@redborder.com>
2024-09-25 10:37:16 +0100

    Correctly check cgroup config on chef run

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -28,9 +28,13 @@ action :add do
     ruby_block 'Checkupdate_cgroups' do
       # Check if active memservices have cgroups assigned and fix all if any
       block do
-        are_cgroups_assigned = `/usr/lib/redborder/scripts/rb_check_cgroups.rb`.strip
-        unless are_cgroups_assigned
-          `/usr/lib/redborder/bin/rb_configure_cgroups.sh`
+        # Run the check script and capture the exit status
+        system('/usr/lib/redborder/scripts/rb_check_cgroups.rb')
+        exit_status = $?.exitstatus
+
+        # Check if the exit status is 1; if true, run the configuration script
+        if exit_status == 1
+          system('/usr/lib/redborder/bin/rb_configure_cgroups.sh')
         end
       end
     end
