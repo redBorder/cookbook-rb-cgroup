effb801ac05a4f4708d151503932f2b8653ac6c6
manegron <manegron@redborder.com>
2024-10-12 00:45:35 +0100

    Make check_cgroups optional (true by default)

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -25,17 +25,19 @@ action :add do
       notifies :run, 'execute[load_cgroups]', :delayed
     end
 
-    # Executed on chef-client
-    ruby_block 'Checkupdate_cgroups' do
-      # Check if active memservices have cgroups assigned and fix all if any
-      block do
-        # Run the check script and capture the exit status
-        system('/usr/lib/redborder/scripts/rb_check_cgroups.rb')
-        exit_status = $CHILD_STATUS.exitstatus
+    if check_cgroups
+      # Executed on chef-client
+      ruby_block 'Checkupdate_cgroups' do
+        # Check if active memservices have cgroups assigned and fix all if any
+        block do
+          # Run the check script and capture the exit status
+          system('/usr/lib/redborder/scripts/rb_check_cgroups.rb')
+          exit_status = $CHILD_STATUS.exitstatus
 
-        # Check if the exit status is 1; if true, run the configuration script
-        if exit_status == 1
-          system('/usr/lib/redborder/bin/rb_configure_cgroups.sh')
+          # Check if the exit status is 1; if true, run the configuration script
+          if exit_status == 1
+            system('/usr/lib/redborder/bin/rb_configure_cgroups.sh')
+          end
         end
       end
     end
